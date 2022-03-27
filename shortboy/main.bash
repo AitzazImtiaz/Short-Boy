1206099b8510fc4b3d1eeefaff8863f223768d87
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-03-27 17:35:31 +0500

    Create main.bash

---
+++ shortboy/main.bash
@@ -0,0 +1,40 @@
+echo "
+  _____ _                _   ____              
+ / ____| |              | | |  _ \             
+| (___ | |__   ___  _ __| |_| |_) | ___  _   _ 
+ \___ \| '_ \ / _ \| '__| __|  _ < / _ \| | | |
+ ____) | | | | (_) | |  | |_| |_) | (_) | |_| |
+|_____/|_| |_|\___/|_|   \__|____/ \___/ \__, |
+                                          __/ |
+                                         |___/" | lolcat
+echo "Shortening URL, that's it the hell" | lolcat
+echo "Designed by Aitzaz Imtiaz, another Boy" | lolcat
+echo "Useful to say, This isn't a kingdom but a virtual Boydom" |lolcat
+echo "People who are in Boydom are stroke of a stupid" | lolcat
+echo ""
+echo ""
+echo "Use for Positive purpose since short Boy is weak and can" | lolcat
+echo "not free you from jail." | lolcat
+echo ""
+echo "Select a weird option:" | lolcatecho "
+  _____ _                _   ____              
+ / ____| |              | | |  _ \             
+| (___ | |__   ___  _ __| |_| |_) | ___  _   _ 
+ \___ \| '_ \ / _ \| '__| __|  _ < / _ \| | | |
+ ____) | | | | (_) | |  | |_| |_) | (_) | |_| |
+|_____/|_| |_|\___/|_|   \__|____/ \___/ \__, |
+                                          __/ |
+                                         |___/" | lolcat
+echo "Shortening URL, that's it the hell" | lolcat
+echo "Designed by Aitzaz Imtiaz, another Boy" | lolcat
+echo "Useful to say, This isn't a kingdom but a virtual Boydom" |lolcat
+echo "People who are in Boydom are stroke of a stupid" | lolcat
+echo ""
+echo ""
+echo "Use for Positive purpose since short Boy is weak and can" | lolcat
+echo "not free you from jail." | lolcat
+echo ""
+echo "Select a weird option from:" | lolcat
+echo ""
+echo "1) Use Bitly Shortner" | lolcat
+echo "2) Use Cuttly Shortener" | lolcat
