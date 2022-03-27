b90b12cc3c5e23922bfce0128e6ffe23f85658b6
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-03-27 18:10:41 +0500

    Update __main__.py

--- shortboy/__main__.py
+++ shortboy/__main__.py
@@ -1,1 +1,23 @@
+import time
+from colorama import Fore
+import os
 
+infinity=1
+
+while infinity==1:
+  os.system("bash main.bash")
+  print("")
+  a = input(Fore.MAGENTA+"Your Option:")
+  if a==1:
+    os.system("python shortboy/scripts/bitly.py")
+  elif a==2:
+    os.system("python shortboy/scripts/cuttly.py")
+  elif a==3:
+    os.system("python shortboy/scripts/aboutme.py")
+  elif a==4:
+    os.system("clear") 
+    exit()
+  else:
+    print(Fore.Red+"Short but wrong 😎")
+    time.sleep(2)
+    os.system("clear")
