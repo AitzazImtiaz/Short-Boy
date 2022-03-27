5fd5bcc8f9c7393c82decfba5b3d89119a7fd639
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-03-27 18:27:42 +0500

    Create aboutme.bash

---
+++ shortboy/scripts/aboutme.bash
@@ -0,0 +1,11 @@
+echo "
+
+ .d8b.  d888888b d888888b d88888D  .d8b.  d88888D 
+d8' `8b   `88'   `~~88~~' YP  d8' d8' `8b YP  d8' 
+88ooo88    88       88       d8'  88ooo88    d8'  
+88~~~88    88       88      d8'   88~~~88   d8'   
+88   88   .88.      88     d8' db 88   88  d8' db 
+YP   YP Y888888P    YP    d88888P YP   YP d88888P " | lolcat
+echo "I am a Boy from Boydom" | lolcat
+echo "Got Bad, Short, currency and many boys!" | lolcat
+echo "Licensed under MIT" | lolcat 
