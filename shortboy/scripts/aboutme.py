6caf354ec58d68f8f1d6d20314baf4028463b82a
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-03-27 18:31:00 +0500

    Update aboutme.py

--- shortboy/scripts/aboutme.py
+++ shortboy/scripts/aboutme.py
@@ -1,6 +1,6 @@
 import os
 os.system("clear)
-os.system("bash aboutme.bash")
+os.system("bash shortboy/scripts/aboutme.bash")
 n = input("Enter y to return:")
 if n=="y":
   os.system("python shortboy/__main__.py")
